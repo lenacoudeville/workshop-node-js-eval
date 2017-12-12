7514f7ac15bb521081209ca5b2ffd820e7a53264
lenacoudeville <lena.coudeville@ynov.com>
2017-12-12 16:50:11 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,18 @@
 exports.run = function(input) {
   console.log("Exercise #3");
+
+  var res = 0;
+  
+  input.forEach(function (item){
+  	if (item.operator=='add'){
+  		res.push('+');
+  	}
+  	if (item.operator=='substract'){
+  		res.push('-');
+  	}
+  	res.push(item.value);
+  })
+
+  parseInt(res);
+  return res;
 };
