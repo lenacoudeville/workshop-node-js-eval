c44a2fb59102befb73e0f3e27eca329a95114e68
lenacoudeville <lena.coudeville@ynov.com>
2017-12-12 16:21:42 +0100

    Exercise 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,18 @@
 exports.run = function(input) {
   console.log("Exercise #2");
+
+  var newTable;
+  var numberArray = input.split("");
+
+  numberArray.forEach(function(element) {
+  console.log(element);
+  if (element<=6){
+  	newTable.push(element);
+  }
+	});
+	
+  	newNumber = Number(newTable);
+	return newNumber;
 };
+
+
